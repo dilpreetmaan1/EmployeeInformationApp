b8a3b68ec831b14c96bf9077b59a190a5c73cff4
dilpreetmaan1999@gmail.com <dilpreetmaan1999@gmail.com>
2021-06-13 11:37:37 +0300

    Add project files.

---
+++ EmployeeInformationApp/EmployeeInformationApp/ISQLite.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeInformationApp.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeInformationApp
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+        bool SaveEmployee(Employee employee);
+        List<Employee> GetEmployees();
+        bool UpdateEmployee(Employee employee);
+        void DeleteEmployee(int id);
+
+    }
+}
